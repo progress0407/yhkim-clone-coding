2a6dc5ea2daf14d37412d901047d3fb917f93fca
swcho <progress0407@naver.com>
2021-08-08 20:22:36 +0900

    spring mvc 전환 v2

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,15 +1,30 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@ServletComponentScan
+@ServletComponentScan // 서블릿 자동등록
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+		return new SpringMemberFormControllerV1();
+	}
+
+
+/*	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}*/
 
 }
