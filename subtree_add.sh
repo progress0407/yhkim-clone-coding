c064f3ebb07c105ca1f8d24c8a6a340f4c9cbd4a
progress0407 <progress0407@gmail.com>
2023-07-10 00:22:03 +0900

    edit subtree_add.sh

--- subtree_add.sh
+++ subtree_add.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 # Check for correct number of arguments
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 {repository name} {repository url} {target directory name} {branch name}"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 {repository name} {repository url}"
     exit 1
 fi
 
 # Assign arguments to variables for better readability
 repo_name=$1
 repo_url=$2
-target_dir=$3
-branch_name=$4
+target_dir=$repo_name
+branch_name="main"
 
 # Add the remote
 git remote add $repo_name $repo_url
+echo "add remote git repo"
 
 # Add the subtree
 git subtree add --prefix=$target_dir $repo_name $branch_name
+echo "add subtree"
 
 # Add and commit changes
 git add -A
 git commit -m "add $repo_name"
+echo "commit"
 
 # Remove the remote
 git remote remove $repo_name
+echo "remove git repo for subtree"
